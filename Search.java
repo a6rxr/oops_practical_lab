642732644281965320ca3e50148ff30307493c0c
abrar <abrar7.md@outlook.com>
2025-08-02 10:46:45 +0530

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,12 @@
+package oops_skill_lab;
+
+public class Search 
+{
+	public static void main (String[] args) {
+		String s = "Hello_world";
+		System.out.println(s.indexOf("_"));
+		System.out.println(s.lastIndexOf("or"));
+		System.out.println(s.contains("ll"));
+	}
+
+}
